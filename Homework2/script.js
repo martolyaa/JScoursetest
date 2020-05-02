92ba7866a16dae87ddc3b56498d95cae20f05fed
Olga <martolyaa@gmail.com>
2020-05-02 17:23:29 +0300

    Работа 2

---
+++ Homework2/script.js
@@ -0,0 +1,43 @@
+let time, money;
+money = +prompt ("Ваш бюджет на месяц?");
+time = prompt ("Введите дату в формате YYYY-MM-DD");
+ 
+let appData = {
+    budget : money,
+    timeData : time,
+    expenses : {},
+    optionalExpenses : {},
+    income : [],
+    savings : false
+};
+ 
+let a = prompt("Введите обязательную статью расходов в этом месяце");
+let b = prompt("Во сколько обойдется?");
+let c = prompt("Введите обязательную статью расходов в этом месяце");
+let d = prompt("Во сколько обойдется?");
+ 
+for (let i = 0; i < 2; i++) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце"),
+	b = prompt("Во сколько обойдется?");
+	
+if ((typeof(a))=== "string" && (typeof(a) != null) && (typeof(b) != null 
+	&& a != "" && b != "" && a.length < 50 && b.length < 50) {
+console.log("done");
+{
+appData.expenses [a] = b;
+}
+};
+
+appData.moneyPerDay = appData.budget /30;
+
+alert("Ежедневный бюджет" + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка")
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+    } else {
+    console.log("Произошла ошибка");
+    }
